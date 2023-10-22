7ebdf21197022e1c1bde5a4806938780f589f51c
ibriangithub <ibriangithub@gmail.com>
2023-10-21 23:21:34 -0300

    Estilos Corregidos preEntrega1

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getProductos } from "../asyncmock";
+import ItemList from "../ItemList/ItemList";
 
 const ItemListContainer = (props) => {
-  return <h2>{props.greeting}</h2>;
+  const [productos, setProductos] = useState([]);
+
+  useEffect(() => {
+    getProductos()
+      .then((respuesta) => setProductos(respuesta))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <>
+      <h2 style={{ textAlign: "center" }}> Mis Productos</h2>;
+      <ItemList productos={productos} />;
+    </>
+  );
 };
 
 export default ItemListContainer;
